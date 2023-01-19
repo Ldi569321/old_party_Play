81900926cb0356d71473d005d5b73de94d18109b
ldi569321 <dragon569321@naver.com>
2023-01-19 10:12:49 +0900

    2023/01/19 10:12

--- js/app.js
+++ js/app.js
@@ -9,15 +9,38 @@ modal.addEventListener("click", () => {
 })
 
 var now_utc = Date.now()
-var timeOff = new Date().getTimezoneOffset()*60000;
-var today = new Date(now_utc-timeOff).toISOString().split("T")[0];
+var timeOff = new Date().getTimezoneOffset() * 60000;
+var today = new Date(now_utc - timeOff).toISOString().split("T")[0];
 
-var maxDateY = parseInt(today.substring(0, 4))+1;
+var maxDateY = parseInt(today.substring(0, 4)) + 1;
 
-const maxDateYMD = maxDateY+today.substring(4, 10);
+const maxDateYMD = maxDateY + today.substring(4, 10);
 
 document.getElementById('createDate').value = new Date().toISOString().slice(0, 7);
 document.getElementById('createDate').value = new Date().toISOString().substring(0, 10);
 
 document.getElementById('maxDate').setAttribute("min", today);
 document.getElementById('maxDate').setAttribute("max", maxDateYMD);
+
+const getDateDiff = (d1, d2) => {
+    const date1 = new Date(d1);
+    const date2 = new Date(d2);
+
+    const diffDate = date1.getTime() - date2.getTime();
+
+    return Math.abs(diffDate / (1000 * 60 * 60 * 24));
+}
+
+let payCal = document.getElementById("payCalculate");
+
+const calBtn = document.getElementById("calculateBtn");
+calBtn.addEventListener("click", () => {
+    if (document.getElementById("dayPay").value == '' || document.getElementById("maxDate").value == '') {
+        alert("필수값이 아직 입력되지 않았습니다.")
+    } else {
+        let dayPay = document.getElementById("dayPay").value;
+        let dayCount = getDateDiff(document.getElementById("createDate").value, document.getElementById("maxDate").value);
+        let result = dayCount * dayPay;
+        payCal.innerText = `${dayCount}일 × ${dayPay}원 = ${result}원`
+    }
+})
